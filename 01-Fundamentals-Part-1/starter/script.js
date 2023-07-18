28c95ba489c608b5b4e22dbb8e9d69c8c421d5c3
aniket30i <iamaniket.saha.09@protonmail.com>
2023-07-19 00:47:55 +0530

    day 3 of learning js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,14 @@ let js='amazing';
 
       40+8+23-10;
       console.log(40+8+23-10);
+      console.log("Aniket");
+      console.log(23);
+
+      let firstName = "Aniket";
+      console.log(firstName);
+
+      console.log(2**3);
+      //** is ^ (power) in javascript */
+
+      const lastName = "Saha";
+      console.log(firstName+' '+lastName);
