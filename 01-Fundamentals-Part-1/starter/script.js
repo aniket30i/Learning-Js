6d2f41b08e8bd5619f45fcf025f2b07944421fdf
aniket30i <iamaniket.saha.09@protonmail.com>
2023-07-20 00:48:52 +0530

    day 5

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,16 +1,27 @@
-let js='amazing';
-      if(js==='amazing') alert('Javascript is fun!');
+// let js='amazing';
+//       if(js==='amazing') alert('Javascript is fun!');
 
-      40+8+23-10;
-      console.log(40+8+23-10);
-      console.log("Aniket");
-      console.log(23);
+      // 40+8+23-10;
+      // console.log(40+8+23-10);
+      // console.log("Aniket");
+      // console.log(23);
 
       let firstName = "Aniket";
-      console.log(firstName);
+      // console.log(firstName);
 
-      console.log(2**3);
-      //** is ^ (power) in javascript */
+      // console.log(2**3);
+      // //** is ^ (power) in javascript */
 
       const lastName = "Saha";
-      console.log(firstName+' '+lastName);
+      // console.log(firstName+' '+lastName);
+
+      // const Aniket = `I'm ${firstName}, a ${2022-1999} year old ${lastName}`;
+      // console.log(Aniket);
+
+      const age=19;
+      const isOldEnough=age>=18;
+
+      if(isOldEnough)
+      {
+            console.log('Sarah can start driving license 😉');
+      }
