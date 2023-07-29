14ca9af53e8b6bae95d34fbee2e761f7cbaed641
aniket30i <iamaniket.saha.09@protonmail.com>
2023-07-29 17:35:43 +0530

    Fundamentals 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+// secure mode and avoid introduction of bug
+
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// if(passTest) hasDriverLicense = true;
+// if(hasDriversLicense) console.log('I can drive : D');
+
+function logger()
+{
+    console.log('My name is Aniket');
+}
+
+logger();
+logger();
+logger();
+
+function fruitProcessor(apple , oranges)
+{
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+fruitProcessor(20,15);
