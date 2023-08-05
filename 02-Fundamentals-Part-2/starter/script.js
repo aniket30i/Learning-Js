8ce93f43fb9799ca512380dcdea1d42fa766d72b
aniket30i <iamaniket.saha.09@protonmail.com>
2023-08-05 23:41:28 +0530

    Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -8,46 +8,64 @@
 // if(passTest) hasDriverLicense = true;
 // if(hasDriversLicense) console.log('I can drive : D');
 
-function logger()
-{
-    console.log('My name is Aniket');
-}
+// function logger()
+// {
+//     console.log('My name is Aniket');
+// }
 
-logger();
-logger();
-logger();
+// logger();
+// logger();
+// logger();
 
-function fruitProcessor(apples , oranges)
-{
-    console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-}
+// function fruitProcessor(apples , oranges)
+// {
+//     console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
 
-const appleJuice = fruitProcessor(20,15);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(20,15);
+// console.log(appleJuice);
 
-// Anonymous function
+// // Anonymous function
 
-const calcAge = function(birthyear)
-{
-    return 2037-birthyear;
-}
-const age = calcAge(1991);
+// const calcAge = function(birthyear)
+// {
+//     return 2037-birthyear;
+// }
+// const age = calcAge(1991);
 
-console.log(age);
+// console.log(age);
 
 
-//arrow function
-const calcAge3 = birthyear => 2037 - birthyear;
-const age2= calcAge3(1992);
-console.log(age2);
+// //arrow function
+// const calcAge3 = birthyear => 2037 - birthyear;
+// const age2= calcAge3(1992);
+// console.log(age2);
 
-const yearUntilRetirement = birthyear =>
-{
-    const age=2037-birthyear;
-    const retirement=65 - age ;
-    return retirement;
-}
+// const yearUntilRetirement = birthyear =>
+// {
+//     const age=2037-birthyear;
+//     const retirement=65 - age ;
+//     return retirement;
+// }
 
-console.log(yearUntilRetirement(1991));
+// console.log(yearUntilRetirement(1991));
+
+
+//Arrays
+
+const friends = [ 'michael' , 'steven' , 'peter'];
+console.log(friends);
+
+const years = new Array(1991,1984,2008,2020);
+
+console.log(friends.length);
+
+console.log(friends[friends.length-1]);
+
+const firstName = 'Aniket';
+const Aniket = [ firstName , 'saha' , 2023-1999 , 'Engg', friends];
+
+console.log(Aniket);
+console.log(Aniket.length);
