b58b75e27a34eb122d17dedb4bef4ca7718e5adb
aniket30i <iamaniket.saha.09@protonmail.com>
2023-08-15 03:05:10 +0530

    modal works

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -11,7 +11,22 @@
 // document.querySelector('.guess').value = 23;
 // console.log(document.querySelector('.guess').value);
 
+const Secretnumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = Secretnumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'Bhosarpappu🤬';
+  } else if (guess === Secretnumber) {
+    document.querySelector('.message').textContent = 'Correct Number!🎉';
+  }else if (guess > Secretnumber) {
+    document.querySelector('.message').textContent = 'Too high📈';
+  }
+  }else if (guess < Secretnumber) {
+    document.querySelector('.message').textContent = 'Too Low📉';
+  }
+  
 });
