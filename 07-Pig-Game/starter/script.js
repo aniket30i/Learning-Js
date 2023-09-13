56a6b4b6da8e4582f9a88a5e1dd6269b93dcb8cf
aniket30i <iamaniket.saha.09@protonmail.com>
2023-09-14 02:48:22 +0530

    current scores and players switch

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -16,6 +18,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -34,8 +37,18 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    //remove or adds the player--active class between either players
+
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
