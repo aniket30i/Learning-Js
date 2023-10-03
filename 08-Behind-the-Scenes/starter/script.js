d34e9eb181d1d09b95b46511c640da645ac46066
aniket30i <iamaniket.saha.09@protonmail.com>
2023-10-03 12:30:19 +0530

    hoisting

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,2 +1,34 @@
 'use strict';
-New OUtput
+
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+var me = 'Aniket';
+let job = 'Engineer';
+const year = 1991;
+
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// const addArrow = (a, b) => a + b;
+
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
