0f77a75accf93a0e38654f6638042e801506eb3f
aniket30i <iamaniket.saha.09@protonmail.com>
2023-10-05 21:15:18 +0530

    nss

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,34 +1,47 @@
 'use strict';
 
-console.log(me);
-//console.log(job);
-//console.log(year);
+// console.log(me);
+// //console.log(job);
+// //console.log(year);
 
-var me = 'Aniket';
-let job = 'Engineer';
-const year = 1991;
+// var me = 'Aniket';
+// let job = 'Engineer';
+// const year = 1991;
 
-// console.log(addDecl(2, 3));
-// console.log(addExpr(2, 3));
-// console.log(addArrow(2, 3));
+// // console.log(addDecl(2, 3));
+// // console.log(addExpr(2, 3));
+// // console.log(addArrow(2, 3));
 
-// function addDecl(a, b) {
-//   return a + b;
+// // function addDecl(a, b) {
+// //   return a + b;
+// // }
+
+// // const addExpr = function (a, b) {
+// //   return a + b;
+// // };
+
+// // const addArrow = (a, b) => a + b;
+
+// if (!numProducts) deleteShoppingCart();
+// var numProducts = 10;
+
+// function deleteShoppingCart() {
+//   console.log('All products deleted');
 // }
 
-// const addExpr = function (a, b) {
-//   return a + b;
-// };
+// var x = 1;
+// let y = 2;
+// const z = 30;
 
-// const addArrow = (a, b) => a + b;
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
 
-if (!numProducts) deleteShoppingCart();
-var numProducts = 10;
-
-function deleteShoppingCart() {
-  console.log('All products deleted');
-}
-
-var x = 1;
-let y = 2;
-const z = 30;
+jonas.greet();
+console.log(this.firstName);
