088e5ebcdb8f71163b4f248af9ae945edcd54b7f
aniket30i <iamaniket.saha.09@protonmail.com>
2023-11-25 17:54:18 +0530

    starting slider

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -120,7 +120,7 @@ tabsContainer.addEventListener('click', function (e) {
 // Menu fade animation
 
 const handleHover = function (e, opacity) {
-  console.log(this, e.currentTarget);
+  //console.log(this, e.currentTarget);
 
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
@@ -157,7 +157,7 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  //console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
@@ -168,6 +168,56 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Reveal sections
+const allSections = document.querySelectorAll('.section');
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+  else entry.target.classList.remove('section--hidden');
+
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+  rootMargin: `-${navHeight}px`,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+//////////////
+// lazy loading images
+
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  //console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  // replace the source of the image access from src to data-src
+  else entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img => imgObserver.observe(img));
+
+// SLIDER
 
 // creating and inserting elements
 // const header = document.querySelector('.header');
